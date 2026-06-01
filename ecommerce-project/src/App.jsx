26e03b713bf1de030694e405db90a2a6fc182a1d
River Pierson <piersonriver@icloud.com>
2026-06-01 08:47:03 +0200

    multiple-pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,14 @@
-import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>Test checkout page</div>}></Route>
+    </Routes>
   )
 }
 
